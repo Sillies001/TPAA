ff9df0acfa29656ac8b650a3687142c99ffe465b
Sillies001 <nwnuwangxue@163.com>
2026-09-24 12:34:00 +0800

    M1-DATA-003: expose Session Time contracts

--- src/tpaa_registry/__init__.py
+++ src/tpaa_registry/__init__.py
@@ -10,12 +10,37 @@ from .source_registration import (
     register_synthetic_fixture,
 )
 
+from .time_alignment import (
+    CanonicalTimeTransformProjection,
+    SessionTimeError,
+    SessionTimeProjection,
+    SessionTimedSourceMarker,
+    SessionTimedSourceRow,
+    apply_session_time_us,
+    build_all_session_time_projections,
+    build_session_time_projection,
+    parse_offset_us,
+    parse_source_time_us,
+    validate_source_clock_sequence,
+)
+
 __all__ = [
     "ImmutableArtifactFileRef",
     "ImmutableHashRef",
     "SourceRegistryError",
     "SourceRegistryIndex",
     "SourceRegistryRegistration",
+    "CanonicalTimeTransformProjection",
+    "SessionTimeError",
+    "SessionTimeProjection",
+    "SessionTimedSourceMarker",
+    "SessionTimedSourceRow",
+    "apply_session_time_us",
+    "build_all_session_time_projections",
+    "build_session_time_projection",
+    "parse_offset_us",
+    "parse_source_time_us",
     "register_all_synthetic_fixtures",
     "register_synthetic_fixture",
+    "validate_source_clock_sequence",
 ]
