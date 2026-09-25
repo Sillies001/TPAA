9e6ebce617805845c03ccf89f2133dd5e8b73783
Sillies001 <nwnuwangxue@163.com>
2026-09-25 09:54:45 +0800

    Export M1 FastAPI backend

--- src/tpaa_api/__init__.py
+++ src/tpaa_api/__init__.py
@@ -2,5 +2,6 @@
 
 from .app import create_app
 from .desktop import create_desktop_app
+from .m1_app import create_m1_app
 
-__all__ = ["create_app", "create_desktop_app"]
+__all__ = ["create_app", "create_desktop_app", "create_m1_app"]
