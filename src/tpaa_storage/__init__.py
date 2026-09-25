b9425a5ffccb1e914b544e9aa307b9ef0251f31a
Sillies001 <nwnuwangxue@163.com>
2026-09-25 09:51:12 +0800

    Keep publication adapters out of storage package import surface

--- src/tpaa_storage/__init__.py
+++ src/tpaa_storage/__init__.py
@@ -8,19 +8,7 @@ from .bootstrap import (
     postgres_create_statements,
     verify_sqlite,
 )
-from .object_seal import LocalSealedObjectFlow, ObjectSealError, StagedObject
 from .ports import BaselineMetadataRepository, RepositoryBaselineMetadata, RepositoryUnitOfWork
-from .publication import (
-    InMemorySessionPublicationRepository,
-    PublishedReleaseConflict,
-    PublishedSessionRelease,
-    PublicationRepositoryError,
-    PublishCASConflict,
-    PublishIdempotencyConflict,
-    PublishResult,
-    ReleaseNotFound,
-    SessionPublicationRepository,
-)
 from .sqlite_repository import (
     SQLiteBaselineMetadataRepository,
     SQLiteDesktopUnitOfWork,
@@ -33,23 +21,11 @@ __all__ = [
     "BaselineMetadataRepository",
     "BootstrapError",
     "BootstrapVerification",
-    "InMemorySessionPublicationRepository",
-    "LocalSealedObjectFlow",
-    "ObjectSealError",
-    "PublicationRepositoryError",
-    "PublishedReleaseConflict",
-    "PublishedSessionRelease",
-    "PublishCASConflict",
-    "PublishIdempotencyConflict",
-    "PublishResult",
-    "ReleaseNotFound",
     "RepositoryBaselineMetadata",
     "RepositoryUnitOfWork",
     "SQLiteBaselineMetadataRepository",
     "SQLiteDesktopUnitOfWork",
     "SQLiteRepositoryError",
-    "SessionPublicationRepository",
-    "StagedObject",
     "bootstrap_sqlite",
     "postgres_create_statements",
     "sqlite_repository_smoke",
