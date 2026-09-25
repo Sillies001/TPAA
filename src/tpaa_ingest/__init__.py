31c34b930c8abf779d65130e56055a292bb04a59
Sillies001 <nwnuwangxue@163.com>
2026-09-25 16:24:26 +0800

    Export M2 reference truth contracts

--- src/tpaa_ingest/__init__.py
+++ src/tpaa_ingest/__init__.py
@@ -1,5 +1,14 @@
 """TPAA ingest-layer public contracts."""
 
+from tpaa_ingest.m2_reference_truth import (
+    M2ReferenceTruthError,
+    M2ReferenceTruthProjection,
+    ReferenceFrameProvenance,
+    ReferenceIdentity,
+    ReferenceTruthRow,
+    load_m2_reference_truth,
+)
+
 from tpaa_ingest.source_adapter import (
     GOVERNED_FIXTURE_IDS,
     SourceArtifactIdentity,
@@ -14,6 +23,11 @@ from tpaa_ingest.source_adapter import (
 
 __all__ = [
     "GOVERNED_FIXTURE_IDS",
+    "M2ReferenceTruthError",
+    "M2ReferenceTruthProjection",
+    "ReferenceFrameProvenance",
+    "ReferenceIdentity",
+    "ReferenceTruthRow",
     "SourceArtifactIdentity",
     "SyntheticAircraftSource",
     "SyntheticSessionSource",
@@ -21,5 +35,6 @@ __all__ = [
     "SyntheticSourceBundle",
     "SyntheticSourceMarker",
     "SyntheticSourceRow",
+    "load_m2_reference_truth",
     "load_synthetic_fixture_bundle",
 ]
